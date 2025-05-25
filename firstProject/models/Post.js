5ded7ad51c7731dd79f107090f608609aeaafe1c
NasrinBanu-J <nasrinnasrin1404@gmail.com>
2025-05-25 22:42:46 +0530

    Update Post.js

--- firstProject/models/Post.js
+++ firstProject/models/Post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema(
       required: true,
     },
     image: {
-      type: String, // Store the image filename
+      type: String,
       default: null,
     },
     author: {
